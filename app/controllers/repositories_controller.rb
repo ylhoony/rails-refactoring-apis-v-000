3f5c26abe1d6d120fa573bec6495aff5e40fcc54
Hoon Lee <yl.hoony@gmail.com>
2018-04-18 02:32:13 +0000

    automatically backed up by learn

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -19,7 +19,7 @@ class RepositoriesController < ApplicationController
 
   private
 
-  def initialize_github
+  def initialize
     @github = GithubService.new( { access_token: session[:token] })
   end
 
