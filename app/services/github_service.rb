3c0329ec4b328b066404fd7fda66691f6af60132
Hoon Lee <yl.hoony@gmail.com>
2018-04-18 02:21:18 +0000

    automatically backed up by learn

--- app/services/github_service.rb
+++ app/services/github_service.rb
@@ -24,7 +24,7 @@ class GithubService
   def get_repos
     response = Faraday.get "https://api.github.com/user/repos", {}, {'Authorization' => "token #{self.access_token}", 'Accept' => 'application/json'}
     repos_array = JSON.parse(response.body)
-    binding.pry
+    # binding.pry
     repos_array.map{|repo| GithubRepo.new(repo) }
   end
 
