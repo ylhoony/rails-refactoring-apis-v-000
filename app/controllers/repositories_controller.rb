e2910ebe52399b6eba084e3a21f0962ba30f8812
Hoon Lee <yl.hoony@gmail.com>
2018-04-18 02:26:59 +0000

    automatically backed up by learn

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -8,7 +8,7 @@ class RepositoriesController < ApplicationController
   end
 
   def create
-
+    github = GithubService.new(params[:name])
     response = Faraday.post "https://api.github.com/user/repos", {name: params[:name]}.to_json, {'Authorization' => "token #{session[:token]}", 'Accept' => 'application/json'}
     redirect_to '/'
   end
