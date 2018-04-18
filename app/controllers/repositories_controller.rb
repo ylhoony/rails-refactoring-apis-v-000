1a70180acf510062345f3eed0d60a5d4f201311c
Hoon Lee <yl.hoony@gmail.com>
2018-04-18 02:37:36 +0000

    Done.

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -1,11 +1,26 @@
 class RepositoriesController < ApplicationController
+  before_action :set_github
+
   def index
-    response = Faraday.get "https://api.github.com/user/repos", {}, {'Authorization' => "token #{session[:token]}", 'Accept' => 'application/json'}
-    @repos_array = JSON.parse(response.body)
+    # response = Faraday.get "https://api.github.com/user/repos", {}, {'Authorization' => "token #{session[:token]}", 'Accept' => 'application/json'}
+    # @repos_array = JSON.parse(response.body)
+    # binding.pry
+
+    # github = GithubService.new( { "access_token": session[:token] })
+    @repos_array = @github.get_repos
   end
 
   def create
-    response = Faraday.post "https://api.github.com/user/repos", {name: params[:name]}.to_json, {'Authorization' => "token #{session[:token]}", 'Accept' => 'application/json'}
+    # github = GithubService.new( { "access_token": session[:token] })
+    @github.create_repo(params[:name])
+    # response = Faraday.post "https://api.github.com/user/repos", {name: params[:name]}.to_json, {'Authorization' => "token #{session[:token]}", 'Accept' => 'application/json'}
     redirect_to '/'
   end
+
+  private
+
+  def set_github
+    @github = GithubService.new( { access_token: session[:token] })
+  end
+
 end
--- app/services/github_service.rb
+++ app/services/github_service.rb
@@ -3,7 +3,7 @@ class GithubService
   attr_reader :access_token
 
   def initialize(access_hash = nil)
-    @access_token = access_hash["access_token"] if access_hash
+    @access_token = access_hash[:access_token] if access_hash
   end
 
   def authenticate!(client_id, client_secret, code)
@@ -23,6 +23,7 @@ class GithubService
   def get_repos
     response = Faraday.get "https://api.github.com/user/repos", {}, {'Authorization' => "token #{self.access_token}", 'Accept' => 'application/json'}
     repos_array = JSON.parse(response.body)
+    # binding.pry
     repos_array.map{|repo| GithubRepo.new(repo) }
   end
 
